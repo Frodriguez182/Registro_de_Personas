9779abfe9f7c0b52d9ee0a11fab8b115e64e5126
Fernando Rodriguez <Fejota001@hotmail.com>
2017-03-18 14:28:46 -0500

    Inicio Plantilla

---
+++ app/src/main/java/castro/rodriguez/fernando/registro_de_personas/Princial.java
@@ -0,0 +1,13 @@
+package castro.rodriguez.fernando.registro_de_personas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Princial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_princial);
+    }
+}
